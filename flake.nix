0326d8cfddf741c7a46d06fad2c80402288a92ef
haruki7049 <tontonkirikiri@gmail.com>
2026-01-09 23:28:33 +0900

    Create flakes

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, lib, ... }:
+        {
+          treefmt = {
+            projectRootFile = ".git/config";
+
+            # Nix
+            programs.nixfmt.enable = true;
+
+            # Zig
+            programs.zig.enable = true;
+            settings.formatter.zig.command = lib.getExe pkgs.zig_0_15;
+
+            # GitHub Actions
+            programs.actionlint.enable = true;
+
+            # Markdown
+            programs.mdformat.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              # Compiler
+              pkgs.zig_0_15
+
+              # LSP
+              pkgs.nil
+              pkgs.zls
+            ];
+          };
+        };
+    };
+}
