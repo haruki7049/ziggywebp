3bac33d23135baba00a6d64d5b1f792baf71efe2
haruki7049 <tontonkirikiri@gmail.com>
2026-01-09 23:31:11 +0900

    Create zig repo

---
+++ build.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Library module declaration
+    const lib_mod = b.addModule("ziggywebp", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Library installation
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "ziggywebp",
+        .root_module = lib_mod,
+    });
+    b.installArtifact(lib);
+
+    // Library unit tests
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    // Test step
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+
+    // Docs
+    const docs_step = b.step("docs", "Emit docs");
+    const docs_install = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "share/ziggywebp/docs",
+    });
+    docs_step.dependOn(&docs_install.step);
+}
